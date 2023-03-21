9d79d9805f75f8e9f696aa0138be3ef6ea41740c
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-03-21 12:58:06 +0000

    [add]bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @book = Book.all
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -6,4 +10,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :introduction)
 end
