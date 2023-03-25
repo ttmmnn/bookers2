04dcf1f1f9f94f06508b8faa12f3a7548385cff7
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-03-25 11:57:24 +0000

    [update]migration_file

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2023_03_20_035334) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
